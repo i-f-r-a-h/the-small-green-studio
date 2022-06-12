631cf740d281e958bffaec5a3228d5a54920fcac
i-f-r-a-h <ifrahvermeer2@gmail.com>
2022-06-12 15:23:03 +0100

    add tailwind example

--- config.js
+++ config.js
@@ -1,27 +1,6 @@
 module.exports = {
 	config: {
-		tailwindjs: "./tailwind.config.js",
+		tailwindjs: "tailwind.config.js",
 		port: 9050
 	},
-	paths: {
-		root: "./",
-		src: {
-			base: "./app",
-			css: "./app/scss",
-			js: "./app/js",
-			img: "./app/assets/img"
-		},
-		dist: {
-			base: "./app",
-			css: "./app/scss",
-			js: "./app/js",
-			img: "./app/assets/img"
-		},
-		build: {
-			base: "./app",
-			css: "./app/scss",
-			js: "./app/js",
-			img: "./app/assets/img"
-		}
-	}
 }
