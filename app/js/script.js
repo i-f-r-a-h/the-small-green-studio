6934d8d38394487bd319a519c7c5548e829ca19d
i-f-r-a-h <ifrahvermeer2@gmail.com>
2022-06-17 13:18:47 +0100

    header

--- app/js/script.js
+++ app/js/script.js
@@ -4,3 +4,18 @@ console.log('HELLO');
 const test = () => {
 	console.log('this is a test');
 };
+
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
+const menuLink = document.querySelector('.navbar__list-link');
+const a = document.querySelectorAll('a');
+//when the hamburger is clicked, menu opens
+menuIcon.addEventListener('click', () => {
+	navbar.classList.toggle('change');
+});
+//when a link is clicked, menu closes
+a.forEach(item => {
+	item.addEventListener('click', () => {
+		navbar.classList.remove('change');
+	});
+});
