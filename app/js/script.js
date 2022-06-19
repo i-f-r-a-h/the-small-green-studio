ade27bd1d2ad4287e056deacc95231604cf05431
i-f-r-a-h <ifrahvermeer2@gmail.com>
2022-06-19 13:05:57 +0100

    add video

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,12 @@
 const menuIcon = document.querySelector('.nav__menu');
 const navbar = document.querySelector('.menu__layout');
 const menuLink = document.querySelector('.navbar__list-link');
+const header = document.querySelector('.site-nav-js');
 const a = document.querySelectorAll('a');
 //when the hamburger is clicked, menu opens
 menuIcon.addEventListener('click', () => {
 	navbar.classList.toggle('change');
+	header.classList.toggle('change-nav');
 });
 //when a link is clicked, menu closes
 a.forEach(item => {
@@ -12,3 +14,26 @@ a.forEach(item => {
 		navbar.classList.remove('change');
 	});
 });
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+// --- ORANGE PANEL ---
+
+const tl = gsap.timeline({
+		scrollTrigger: {
+			trigger: ".orangeWrapper",
+			scrub: true,
+			pin: true,
+
+			start: "50% 50%",
+			end: "+=100%"
+		}
+	})
+
+	.from(".orange", {
+		scale: 0.2,
+		ease: "none"
+	})
