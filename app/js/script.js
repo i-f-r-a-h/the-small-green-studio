946887167da57761cb27ad55568c806b2e8297f3
i-f-r-a-h <ifrahvermeer2@gmail.com>
2022-06-20 18:30:18 +0100

    add new menu design

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,19 @@
-const menuIcon = document.querySelector('.nav__menu');
-const navbar = document.querySelector('.menu__layout');
-const menuLink = document.querySelector('.navbar__list-link');
-const header = document.querySelector('.site-nav-js');
-const a = document.querySelectorAll('a');
-//when the hamburger is clicked, menu opens
-menuIcon.addEventListener('click', () => {
-	navbar.classList.toggle('change');
-	header.classList.toggle('change-nav');
-});
-//when a link is clicked, menu closes
-a.forEach(item => {
-	item.addEventListener('click', () => {
-		navbar.classList.remove('change');
-	});
-});
+// const menuIcon = document.querySelector('.nav__menu');
+// const navbar = document.querySelector('.menu__layout');
+// const menuLink = document.querySelector('.navbar__list-link');
+// const header = document.querySelector('.site-nav-js');
+// const a = document.querySelectorAll('a');
+// //when the hamburger is clicked, menu opens
+// menuIcon.addEventListener('click', () => {
+// 	navbar.classList.toggle('change');
+// 	header.classList.toggle('change-nav');
+// });
+// //when a link is clicked, menu closes
+// a.forEach(item => {
+// 	item.addEventListener('click', () => {
+// 		navbar.classList.remove('change');
+// 	});
+// });
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -47,3 +47,68 @@ const tl = gsap.timeline({
 		opacity: 1,
 		ease: "none"
 	})
+
+
+
+
+
+
+// menu
+const menu_open = gsap.timeline({
+	paused: "true",
+	reversed: "true",
+});
+menu_open.to(".menu-container", {
+	display: "flex",
+});
+menu_open.to("#menuBtnDiv", {
+	duration: 0.2,
+	y: 50,
+});
+menu_open.fromTo(
+	"#menuBtnDiv2", {
+		duration: 0.2,
+		y: -100,
+	}, {
+		y: -50,
+	},
+	"-=.15"
+);
+menu_open.from(
+	".menu-container-1", {
+		duration: 0.5,
+		y: "100%",
+	},
+	"-=.5"
+);
+menu_open.from(
+	".menu-container-1 .menu-container-header li span, .menu-container-1 .menu-container-footer li span", {
+		stagger: {
+			amount: 0.15,
+		},
+		y: 100,
+		duration: 0.5,
+	},
+	"-=.1"
+);
+menu_open.from(
+	".menu-container-2", {
+		duration: 0.5,
+		y: "-100%",
+	},
+	"-=1"
+);
+menu_open.from(
+	".menu-container-2 .menu-container-footer li span", {
+		stagger: {
+			amount: 0.15,
+		},
+		y: 100,
+		duration: 0.5,
+	},
+	"-=.5"
+);
+
+function menuOpen() {
+	menu_open.reversed() ? menu_open.play() : menu_open.reverse();
+}
