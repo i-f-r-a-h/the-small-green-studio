c40a55bf26c6d9c2fade403ea60a5fe05c5c083a
i-f-r-a-h <ifrahvermeer2@gmail.com>
2022-06-24 16:32:23 +0100

    feat: add cursor and hover over effect

--- app/js/script.js
+++ app/js/script.js
@@ -115,3 +115,57 @@ tl.to(".hero__content", {
 	opacity: 1,
 	ease: "none"
 });
+
+
+
+// on hover image effect
+// .active--cursor 
+
+//variables
+let cursor = $(".cursor"),
+	follower = $(".cursor__follower");
+
+let posX = 0,
+	posY = 0,
+	mouseX = 0,
+	mouseY = 0;
+
+//cursor change effect
+TweenMax.to({}, 0.016, {
+	repeat: -1,
+	onRepeat: function () {
+		posX += (mouseX - posX) / 9;
+		posY += (mouseY - posY) / 9;
+
+		TweenMax.set(follower, {
+			css: {
+				left: posX - 20,
+				top: posY - 20
+			}
+		});
+
+		TweenMax.set(cursor, {
+			css: {
+				left: mouseX,
+				top: mouseY
+			}
+		});
+
+	}
+});
+
+//event listeners
+$(document).on("mousemove", function (e) {
+	mouseX = e.pageX;
+	mouseY = e.pageY;
+});
+
+$(".team-js img").on("mouseenter", function () {
+	cursor.addClass("active--cursor");
+	follower.addClass("active--cursor");
+});
+
+$(".team-js img").on("mouseleave", function () {
+	cursor.removeClass("active--cursor");
+	follower.removeClass("active--cursor");
+});
