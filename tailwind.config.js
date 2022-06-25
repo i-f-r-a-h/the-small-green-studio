f2ab4d16e7d03f76c6eb247b1c32c767e96e8174
Milesscott <milesbscott@gmail.com>
2022-06-25 15:37:42 +0100

    Project section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        'light-green': '#BEF0BF',
+        'light-brown': '#D9D6C3',
+        'forest-green': '#2A4D4C',
+        'standard-green': '#BEE4AF',
+        'deep-green': '#008000',
+      }
+
+
+
+
+    },
   },
   variants: {
     extend: {},
