40710515a0c6683d18a14f5e148039568237f510
i-f-r-a-h <ifrahvermeer2@gmail.com>
2022-06-30 18:32:55 +0100

    add content

--- app/js/script.js
+++ app/js/script.js
@@ -191,8 +191,154 @@ sections.forEach((sct, i) => {
 
 });
 
+gsap.registerPlugin(Flip);
+window.addEventListener('load', () => {
+	const headshots = gsap.utils.toArray('.leadershipCard'),
+		expandedContainer = document.querySelector('.leadershipExpandedContainer'),
+		expandedBioContainer = document.querySelector('.leadershipExpandedContainer__bio'),
+		expandedImage = document.querySelector('.leadershipExpandedContainer__headshot img'),
+		expandedName = document.querySelector('.leadershipExpandedContainer__bio .name'),
+		expandedTitle = document.querySelector('.leadershipExpandedContainer__bio .title'),
+		expandedBio = document.querySelector('.leadershipExpandedContainer__bio .bio'),
+		expandedSocial = document.querySelector('.leadershipExpandedContainer__bio .social')
+
+	let activeHeadshot;
+
+	gsap.set(expandedBioContainer, {
+		xPercent: 15,
+		opacity: 0
+	});
+
+	function expandHeadshot(card) {
+		//If someone clicks on element behind expanded headshot, then we should 
+		//just close the currently expanded headshot
+		if (activeHeadshot) {
+			return closeHeadshot()
+		}
+
+		//If we're on a small screen, just return. The bios are expanded via css
+		if (window.innerWidth < 800) {
+			return;
+		}
+
+		//When the image in the detail DOM is loaded, we run the GSAP flip stuff.
+		//Anything outside this function isn't related to FLIPing the headshot
+		const onload = () => {
+
+			//Position the expanded container on top of the headshot.
+			//Make sure to use fitChild on the image so that's what looks like
+			//it's growing. 
+			Flip.fit(expandedContainer, card, {
+				scale: true,
+				fitChild: expandedImage
+			})
+
+			//Record the state that everything is in.
+			const state = Flip.getState(expandedContainer);
+
+			//Set the final state
+			gsap.set(expandedContainer, {
+				clearProps: true
+			});
+			gsap.set(expandedContainer, {
+				visibility: 'visible'
+			});
+
+			Flip.from(state, {
+				duration: 0.75,
+				scale: true,
+				ease: "power2.inOut",
+				absolute: true
+			}).to(expandedBioContainer, {
+				xPercent: 0,
+				opacity: 1
+			}, .75)
+
+			expandedImage.removeEventListener('load', onload);
+			document.addEventListener('click', closeHeadshot);
+
+		}
 
 
+		//Change image and text
+		const data = card.dataset;
+		expandedImage.addEventListener('load', onload);
+		expandedImage.src = data.headshot;
+		expandedName.innerText = data.name;
+		expandedTitle.innerText = data.titles;
+		expandedBio.innerText = data.bio;
+
+		//Fade out the other items
+		gsap.to(headshots, {
+			opacity: 0.3,
+			stagger: {
+				amount: 0.7,
+				from: headshots.indexOf(card),
+				grid: 'auto'
+			}
+		});
+		activeHeadshot = card;
+
+	}
+
+	function closeHeadshot() {
+		console.log('Closing headshot');
+		document.removeEventListener('click', closeHeadshot);
+
+		//Record current state
+		const state = Flip.getState(expandedContainer);
+
+		//Scale details down so that it's image fits exactly on top of the active headshot
+		Flip.fit(expandedContainer, activeHeadshot, {
+			scale: true,
+			fitChild: expandedImage
+		});
+
+		//Put the bio container back and then fade in the other headshots
+		const tl = gsap.timeline();
+		tl.to(expandedBioContainer, {
+				xPercent: 15,
+				opacity: 0
+			})
+			.to(expandedContainer, {
+				visibility: 'hidden'
+			})
+			.to(headshots, {
+				opacity: 1,
+				stagger: {
+					amount: 0.7,
+					from: headshots.indexOf(activeHeadshot),
+					grid: 'auto'
+				}
+			});
+
+		Flip.from(state, {
+			scale: true,
+			duration: 0.5,
+			delay: 0.2
+		})
+
+		activeHeadshot = null;
+
+
+	}
+
+	//Add click events
+	gsap.utils.toArray('.leadershipCard').forEach(item => {
+		item.addEventListener('click', () => {
+			expandHeadshot(item);
+		});
+	});
+
+	gsap.set('.leadershipCard', {
+		autoAlpha: 1
+	});
+	gsap.from('.leadershipCard', {
+		opacity: 0,
+		yPercent: 30,
+		stagger: 0.14
+	})
+});
 
 /// introduction date 
 var prnDt = new Date().toLocaleDateString('en-us', {
