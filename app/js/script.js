849e395e5bcf3c42fc9fabac3dd92225297df327
i-f-r-a-h <ifrahvermeer2@gmail.com>
2022-07-08 16:18:29 +0100

    save

--- app/js/script.js
+++ app/js/script.js
@@ -308,3 +308,77 @@ $(".principles__item").on("click", function () {
 	}
 	imageloop();
 });
+
+
+//purpose led projects
+$(document).ready(function () {
+	TweenMax.set(".project-preview", {
+		width: 0
+	});
+
+	var tl = new TimelineLite();
+
+	$(document)
+		.on("mouseover", ".navigation-item", function (evt) {
+			tl = new TimelineLite();
+			tl.to($(".project-preview"), 1, {
+				width: "600px",
+				ease: Expo.easeInOut
+			});
+		})
+		.on("mouseout", ".navigation-item", function (evt) {
+			tl = new TimelineLite();
+			tl.to($(".project-preview"), 0.5, {
+				width: 0,
+				ease: Expo.easeInOut
+			});
+		});
+});
+
+$(".navigation-link-1").hover(function () {
+	$(".project-preview").css({
+		"background-image": "url(img-1.jpg)"
+	});
+});
+
+$(".navigation-link-2").hover(function () {
+	$(".project-preview").css({
+		"background-image": "url(img-2.jpg)"
+	});
+});
+
+$(".navigation-link-3").hover(function () {
+	$(".project-preview").css({
+		"background-image": "url(img-3.jpg)"
+	});
+});
+
+$(".navigation-link-4").hover(function () {
+	$(".project-preview").css({
+		"background-image": "url(img-4.jpg)"
+	});
+});
+
+$(".navigation-link-5").hover(function () {
+	$(".project-preview").css({
+		"background-image": "url(img-5.jpg)"
+	});
+});
+
+$(".navigation-link-6").hover(function () {
+	$(".project-preview").css({
+		"background-image": "url(img-6.jpg)"
+	});
+});
+
+$(".navigation-link-7").hover(function () {
+	$(".project-preview").css({
+		"background-image": "url(img-7.jpg)"
+	});
+});
+
+$(".navigation-link-8").hover(function () {
+	$(".project-preview").css({
+		"background-image": "url(img-8.jpg)"
+	});
+});
